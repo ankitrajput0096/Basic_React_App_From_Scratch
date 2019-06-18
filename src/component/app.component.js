5a1b9322de3030f7e92f6814fec7b22f8929415d
Ankit <ankitrajput0096@gmail.com>
2019-06-18 08:05:15 +0530

    handling image resoruce and 'fire the prod' command implemented

--- src/component/app.component.js
+++ src/component/app.component.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import "../css/main.css";
+import logo from '../assets/saitama.jpg';
+
 class MyComponent extends Component {
   render() {
-    return <div className={"intro"}>Hello World</div>;
+    return (
+    <div>
+      <div className={"intro"}>Hello World</div>
+      <img src={logo} alt="Logo" />
+    </div>
+    );
   }
 }
 export default MyComponent;
